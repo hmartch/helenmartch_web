44fea43c19b60385468b07c1f89bca4bfed629fc
Helen Martch <helen@mobilexlabs.com>
2017-07-20 11:51:33 -0500

    new version of js file

--- includes/foot.php
+++ includes/foot.php
@@ -4,4 +4,4 @@
     </footer>
   
   <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-  <script src="js/hlm2011.js" type="text/javascript"></script>
+  <script src="js/hlm2017.js" type="text/javascript"></script>
