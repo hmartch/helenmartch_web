f37ec2f2e8dc5dca4c237b3fbd5d41c6f7589182
Helen Martch <helen@mobilexlabs.com>
2017-07-21 16:18:18 -0500

    updated resume pdf and links to it

--- includes/general-side1.php
+++ includes/general-side1.php
@@ -1,8 +1,7 @@
-<!-- TODO: update resume link -->
 <p><img src="/images/facebook-15.png" class="icon" alt=""/>Find me on <a href="http://www.facebook.com/profile.php?id=100000912637008" target="ext">Facebook</a>.</p>
     <p><img src="/images/GitHub-Mark-32px.png" class="icon" alt="" />View my <a href="https://github.com/hmartch" target="ext">GitHub</a> profile.</p>
     <p><img src="/images/icon-instagram.png" class="icon" alt="" />See my photos on <a href="https://www.instagram.com/helen.martch/" target="ext">Instagram</a>.</p>
 		<p><img src="/images/linkedin-15.png" class="icon" alt="" />Link up on <a href="http://www.linkedin.com/pub/helen-martch/15/885/931" target="ext">LinkedIn</a>.</p>
     <p><img src="/images/icon-stackoverflow.png" class="icon" alt="" />View my <a href="https://stackoverflow.com/users/1267500/redlena" target="ext">Stack Overflow</a> profile.</p>
     <p><img src="/images/icon-twitter.png" class="icon" alt="" />Follow me on <a href="https://twitter.com/helenmartch" target="ext">Twitter</a>.</p>
-    <!--<p>Download my <a href="pdfs/resume-hmartch-201305-web.pdf" target="ext">r&eacute;sum&eacute;</a>.</p>-->
+    <p>Download my <a href="pdfs/resume-hmartch-201707-web.pdf" target="ext">r&eacute;sum&eacute;</a>.</p>
