0724ebdc1bae9acc583c895d7db23260a7b19d7d
Helen Martch <helen@mobilexlabs.com>
2017-07-21 09:43:27 -0500

    js fixes and cleanup

--- js/hlm2017.js
+++ js/hlm2017.js
@@ -1,6 +1,6 @@
 (function () {
-	/* Declare main query variables */
-	_document = $(document);
+	/* Declare main query variable */
+	var _document = $(document);
 
 	/* Copyright Year */
 	var	copyright = _document.find('#copyright');
@@ -19,7 +19,7 @@
 
 	/* Nav Highlighting */
 	var	pathStr = location.pathname,
-		thisPg = pathStr.substring(pathStr.lastIndexOf('/')+1, pathStr.length);
+			thisPg = pathStr.substring(pathStr.lastIndexOf('/')+1, pathStr.length);
 	if ( thisPg.indexOf('family') > -1 ) {
 		_document.find('#fam').addClass('on');
 	}
@@ -36,7 +36,7 @@
 	_document.find('nav li:last-child').addClass('last-child');
 
 	/* ipad and iphone fix, with thanks to Mark Goddard from http://blog.0100.tv/2010/05/fixing-the-hover-event-on-the-ipadiphoneipod/ */
-	var topnavlink = $('nav ul li a');
+	var topnavlink = _document.find('nav ul li a');
 	if ( (navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) ) {
 		topnavlink.bind('touchstart', function() {
 			//console.log("touch started");
@@ -56,7 +56,7 @@
 		vidcode += 'Sorry, but your browser does not support the video tag.';
 		vidcode += '<\/video>';
 		vidcode += '<p>"The Death of a Snowman"<\/p>';
-		_document.find('#side2').html(vidcode);
+		_document.find('.side.details').html(vidcode);
 	}
 
 	/* Toggle Course Lists */
@@ -71,8 +71,8 @@
 	});
 
 	/* Slideshows */
-	var thumbs = $('div#thumbs');
-	if ( thumbs.length || $('h2.refer').length) {
+	var thumbs = _document.find('div#thumbs');
+	if ( thumbs.length || _document.find('h2.refer').length) {
 		var slides = slidesJSON.slides;
 		var prevshow = '';
 	}
@@ -81,7 +81,7 @@
 		_document.find('.side.details').append('<ul class="slides"></ul>');
 		$(slides).each(function(i) {
 			if ( slides[i].show != prevshow ) {
-				$('.side.details ul').append('<li id="listitem' + i + '"><span>' + slides[i].show + '</span></li>');
+				_document.find('.side.details ul').append('<li id="listitem' + i + '"><span>' + slides[i].show + '</span></li>');
 				return prevshow = slides[i].show;
 			}
 		});
@@ -92,47 +92,47 @@
 		slideshowList();
 
 		/* Slideshow Thumbnails */
-		$('div#thumbs').append('<ul></ul>');
+		_document.find('div#thumbs').append('<ul></ul>');
 		$(slides).each(function(i) {
-			$('div#thumbs ul').append('<li><img src="' + slides[i].path + '-50.jpg" alt="' + slides[i].title + '" id="slide' + i + '"/></li>');
+			_document.find('div#thumbs ul').append('<li><img src="' + slides[i].path + '-50.jpg" alt="' + slides[i].title + '" id="slide' + i + '"/></li>');
 		});
 
-		var list = $('#thumbs li');
+		var list = _document.find('#thumbs li');
 		var slideSwitch = function (id) {
 			var slides = slidesJSON.slides;
 			list.css('opacity','.7');
 			nid = parseInt(id);
-			$('div#main h3:first').text(slides[nid].show);
-			$('img#slideimg').attr('src', slides[nid].path + '-530.jpg').attr('alt', slides[nid].title);
-			$('h3#slidetitle').text(slides[nid].title);
-			$('p#slidedesc').text(slides[nid].description);
-			if ( $('p#slideloc').length ) {
-				$('p#slideloc').text(slides[nid].location);
+			_document.find('div#main h3:first').text(slides[nid].show);
+			_document.find('img#slideimg').attr('src', slides[nid].path + '-530.jpg').attr('alt', slides[nid].title);
+			_document.find('h3#slidetitle').text(slides[nid].title);
+			_document.find('p#slidedesc').text(slides[nid].description);
+			if ( _document.find('p#slideloc').length ) {
+				_document.find('p#slideloc').text(slides[nid].location);
 			}
-			if ( $('p#slidedate').length ) {
-				$('p#slidedate').text(slides[nid].date);
+			if ( _document.find('p#slidedate').length ) {
+				_document.find('p#slidedate').text(slides[nid].date);
 			}
 			var current = $(list[nid]);
 			current.css('opacity','1');
 			if ( nid != '0' ) {
-				$('span.first').addClass('link');
-				$('span.prev').addClass('link');
+				_document.find('span.first').addClass('link');
+				_document.find('span.prev').addClass('link');
 			}
 			if ( nid == '0' ) {
-				$('span.first').removeClass('link');
-				$('span.prev').removeClass('link');
+				_document.find('span.first').removeClass('link');
+				_document.find('span.prev').removeClass('link');
 			}
 			if ( nid != list.length-1 ) {
-				$('span.next').addClass('link');
-				$('span.last').addClass('link');
+				_document.find('span.next').addClass('link');
+				_document.find('span.last').addClass('link');
 			}
 			if ( nid == list.length-1 ) {
-				$('span.next').removeClass('link');
-				$('span.last').removeClass('link');
+				_document.find('span.next').removeClass('link');
+				_document.find('span.last').removeClass('link');
 			}
 		};
 
-		$('nav.slide').on('click', 'span.link', function() {
+		_document.find('nav.slide').on('click', 'span.link', function() {
 			if ( $(this).hasClass('first') ) {
 				nid = 0;
 			}
@@ -153,7 +153,7 @@
 			thumbs.slideLeft(nid);
 		});
 
-		$('div#side3').on('click', 'li', function() {
+		_document.find('.side.details').on('click', 'li', function() {
 			nid = parseInt($(this).attr('id').substring(8, $(this).attr('id').length));
 			thumbs.slideLeft(nid);
 		});
@@ -175,17 +175,17 @@
 	};
 
 	/* Side3 Slideshow List on Other Pages */
-	if ( $('.refer').length ) {
+	if ( _document.find('.refer').length ) {
 		slideshowList();
 	
-		$('div#side3').on('click', 'li', function() {
+		_document.find('.side.details').on('click', 'li', function() {
 			nid = parseInt($(this).attr('id').substring(8, $(this).attr('id').length));
 			name = $(this).children('span').text();
 			setStart(nid,name);
 		});
 	
 		var setStart = function(nid,name) {
-			var page = $('h2:first').attr('id');
+			var page = _document.find('h2:first').attr('id');
 			$(slides).each(function(i) {
 				if ( slides[i].show == name ) {
 					if ( page == "travel" ) {
@@ -208,13 +208,13 @@
 		return this;
 	};
 
-	if ( $('#main #video').length ) {
+	if ( _document.find('#main #video').length ) {
 		var videos = videosJSON.videos;
 		
 		var loadVid = function (id) {
-			var vid = $('#main #video');
+			var vid = _document.find('#main #video');
 			nid = parseInt(id);
-			if ( $('#video').length ) {
+			if ( _document.find('#video').length ) {
 				var video = detectVideoSupport();
 					if ( video.ogg && !video.mp4 ) {
 						vid.attr('src','/videos/' + videos[nid].file + '.theora.ogg');
@@ -222,19 +222,18 @@
 					else {
 						vid.attr('src','/videos/' + videos[nid].file + '.mp4');
 					}
-					$('#desc').html(videos[nid].description);
-					vid.load();
+					_document.find('#desc').html(videos[nid].description);
 				}
 			};
 		loadVid(0);
 
-		/* Side3 Video Thumbnail List on Video Page */
-		$('div#side3').append('<ul class="videos"></ul>');
+		/* Side Video Thumbnail List on Video Page */
+		_document.find('.side.details').append('<ul class="videos"></ul>');
 		$(videos).each(function(i) {
-			$('div#side3 ul').append('<li><img src="' + videos[i].still + '" alt="Click for video" class="thumb" id="video' + i + '"/></li>');
+			_document.find('.side.details ul').append('<li><img src="' + videos[i].still + '" alt="Click for video" class="thumb" id="video' + i + '"/></li>');
 		});
 	
-		$('div#side3 ul.videos').on('click', 'img', function() {
+		_document.find('.side.details ul.videos').on('click', 'img', function() {
 			$this = $(this);
 			nid = parseInt($this.attr('id').substring(5, $this.attr('id').length));
 			loadVid(nid);
@@ -243,23 +242,20 @@
 	
 	/* Portfolio Category */
 	var showCategory = function(i) {
-		$('ul.photos').remove();
+		_document.find('ul.photos').remove();
 		categories = portfolioCategories.categories;
 		projects = portfolioProjects.projects;
 		thisCat = categories[i].catabbrev;
-		//$('title').html('Portfolio: '+categories[i].catname+' - HelenMartch.com');
-		//$('#metadesc').attr('content',categories[i].catdesc);
-		//$('#metakeys').attr('content',categories[i].catkeys);
-		$('.catname').html(categories[i].catname);
-		$('.catdesc').html(categories[i].catlongdesc);
-		$('#main').append('<ul class="photos"></ul>');
+		_document.find('.catname').html(categories[i].catname);
+		_document.find('.catdesc').html(categories[i].catlongdesc);
+		_document.find('#main').append('<ul class="photos"></ul>');
 		$(projects).each(function(j) {
 			if (projects[j].category == thisCat && projects[j].type == 'main'){
 				if (projects[j].groupname == ''){
-					$('#main ul').append('<li><a href="portfolio-project.php?piece='+projects[j].pId+'"><img src="images/portfolio/'+projects[j].category+"/"+projects[j].image+'-165.jpg" alt="' + projects[j].piecename + '" title="'+projects[j].piecename+'"/><br/><span class="piecetitle">'+projects[j].piecename+'</span></a></li>');
+					_document.find('#main ul').append('<li><a href="portfolio-project.php?piece='+projects[j].pId+'"><img src="images/portfolio/'+projects[j].category+"/"+projects[j].image+'-165.jpg" alt="' + projects[j].piecename + '" title="'+projects[j].piecename+'"/><br/><span class="piecetitle">'+projects[j].piecename+'</span></a></li>');
 				}
 				else {
-					$('#main ul').append('<li><a href="portfolio-project.php?piece='+projects[j].pId+'"><img src="images/portfolio/'+projects[j].category+"/"+projects[j].image+'-165.jpg" alt="' + projects[j].piecename + '" title="'+projects[j].piecename+'"/><br/><span class="piecetitle">'+projects[j].groupname+'</span></a></li>');
+					_document.find('#main ul').append('<li><a href="portfolio-project.php?piece='+projects[j].pId+'"><img src="images/portfolio/'+projects[j].category+"/"+projects[j].image+'-165.jpg" alt="' + projects[j].piecename + '" title="'+projects[j].piecename+'"/><br/><span class="piecetitle">'+projects[j].groupname+'</span></a></li>');
 				}
 			}
 		});
@@ -273,50 +269,47 @@
 		while( x-- ) {
     		if( categories[x].catabbrev === thisProjCat ) break;
 		}
-		//$('title').html('Portfolio: '+projects[i].piecename+' - HelenMartch.com');
-		//$('#metadesc').attr('content',projects[i].projdesc);
-		//$('#metakeys').attr('content',projects[i].projkeys + categories[x].catkeys);
 		if (projects[i].groupname == '') {
-			$('h2').html(projects[i].piecename);
-			$('.piece h3').hide();
+			_document.find('h2').html(projects[i].piecename);
+			_document.find('.piece h3').hide();
 			if (projects[i].type == 'main'){
-				$('section.relateds').hide();
+				_document.find('section.relateds').hide();
 			}
 		} else {
-			$('h2').html(projects[i].groupname);
-			$('.piece h3').html(projects[i].piecename);			
+			_document.find('h2').html(projects[i].groupname);
+			_document.find('.piece h3').html(projects[i].piecename);			
 			$(projects).each(function(findRel){
 				if (projects[findRel].type == 'related' || projects[findRel].type == 'source') {
 				if (projects[findRel].groupname == projects[i].groupname) {
-					$('#main').append('<section class="relateds '+projects[findRel].pId+'"><figure class="right"><img alt="'+projects[findRel].piecename+'" title="'+projects[findRel].piecename+'" /></figure><h3></h3><p class="years"></p><p class="piecedesc"></p><p class="mat"></p></section>');
+					_document.find('#main').append('<section class="relateds '+projects[findRel].pId+'"><figure class="right"><img alt="'+projects[findRel].piecename+'" title="'+projects[findRel].piecename+'" /></figure><h3></h3><p class="years"></p><p class="piecedesc"></p><p class="mat"></p></section>');
 					if (projects[findRel].orientation == 'vertical') {
-						$('.'+projects[findRel].pId+' figure').addClass('vertical');
+						_document.find('.'+projects[findRel].pId+' figure').addClass('vertical');
 					} else {
-						$('.'+projects[findRel].pId+' figure').removeClass('vertical');
+						_document.find('.'+projects[findRel].pId+' figure').removeClass('vertical');
 					}
-					$('.'+projects[findRel].pId+' figure img').attr('src','images/portfolio/'+projects[findRel].category+"/"+projects[findRel].image+'-320.jpg');
-					$('.'+projects[findRel].pId+' h3').html(projects[findRel].piecename);
-					$('.'+projects[findRel].pId+' .years').html(projects[findRel].years);
-					$('.'+projects[findRel].pId+' .piecedesc').html(projects[findRel].piecedesc);	
-					$('.'+projects[findRel].pId+' .mat').html(projects[findRel].material);
-					$('.'+projects[findRel].pId).show();
+					_document.find('.'+projects[findRel].pId+' figure img').attr('src','images/portfolio/'+projects[findRel].category+"/"+projects[findRel].image+'-320.jpg');
+					_document.find('.'+projects[findRel].pId+' h3').html(projects[findRel].piecename);
+					_document.find('.'+projects[findRel].pId+' .years').html(projects[findRel].years);
+					_document.find('.'+projects[findRel].pId+' .piecedesc').html(projects[findRel].piecedesc);	
+					_document.find('.'+projects[findRel].pId+' .mat').html(projects[findRel].material);
+					_document.find('.'+projects[findRel].pId).show();
 				}
 				}
 			});
 		}
 		if (projects[i].orientation == 'vertical') {
-			$('.piece figure').addClass('vertical');
+			_document.find('.piece figure').addClass('vertical');
 		} else {
-			$('.piece figure').removeClass('vertical');
+			_document.find('.piece figure').removeClass('vertical');
 		}
-		$('.piece figure').html('<img src="images/portfolio/'+projects[i].category+'/'+projects[i].image+'-320.jpg" title="'+projects[i].piecename+'" alt="'+projects[i].piecename+'"/>');
-		$('.piece .years').html(projects[i].years);
-		$('.piece .piecedesc').html(projects[i].piecedesc);	
-		$('.piece .mat').html(projects[i].material);
-		$('#side3 h3').html('More '+categories[x].catname);
+		_document.find('.piece figure').html('<img src="images/portfolio/'+projects[i].category+'/'+projects[i].image+'-320.jpg" title="'+projects[i].piecename+'" alt="'+projects[i].piecename+'"/>');
+		_document.find('.piece .years').html(projects[i].years);
+		_document.find('.piece .piecedesc').html(projects[i].piecedesc);	
+		_document.find('.piece .mat').html(projects[i].material);
+		_document.find('.side.details h3').html('More '+categories[x].catname);
 		$(projects).each(function(j) {
 			if (projects[j].category == thisProjCat && projects[j].type == 'main'){
-				$('#side3').append('<a href="portfolio-project.php?piece='+projects[j].pId+'"><img src="images/portfolio/'+projects[j].category+"/"+projects[j].image+'-60.jpg" alt="' + projects[j].piecename + '" title="'+projects[j].piecename+'" class="thumb"/></a>');
+				_document.find('.side.details').append('<a href="portfolio-project.php?piece='+projects[j].pId+'"><img src="images/portfolio/'+projects[j].category+"/"+projects[j].image+'-60.jpg" alt="' + projects[j].piecename + '" title="'+projects[j].piecename+'" class="thumb"/></a>');
 			}
 		});
 	}
