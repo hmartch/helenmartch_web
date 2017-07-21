db00029106bea55531bbfdda06ba166c6c3d30ee
Helen Martch <helen@mobilexlabs.com>
2017-07-21 11:37:04 -0500

    just link to main videos page

--- family-memories.php
+++ family-memories.php
@@ -26,20 +26,7 @@
 			</ul>
 
 			<h3><a href="family-videos.php">Videos</a></h3>
-			<p>Mostly of my son, as a baby and young child, but a couple other things too.</p>
-			<ul class="videos">
-				<li><a href=""><img src="/images/stills/20030826-justborn-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20040216-feeding-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20040225-tummytime-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20040313-bouncy-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20040616-books-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20040814-dancing-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20060729-dancing-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20110605-sealions-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20110605-stingraybay-60.jpg" alt=""/><br/></a></li>
-				<li><a href=""><img src="/images/stills/20110605-zoohiding-60.jpg" alt=""/><br/></a></li>
-				<!--<li><a href=""><img src="/images/stills/-60.jpg" alt=""/><br/></a></li>-->
-			</ul>
+			<p>Mostly of my son, as a baby and young child.</p>
 
 		</section>
 	</div>
