dea4aba175135998f7be7c2248ae9e37d38acc57
Helen Martch <helen@mobilexlabs.com>
2017-07-25 13:47:15 -0500

    add og tags and image

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="google-site-verification" content="hgbcQefWUSgp2t9BA0KJuCPJQGyOVdeemJ8h8sy7L9c" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="http://www.helenmartch.com/images/helen-hm.png" />
+  <meta property="og:image:width" content="78" />
+  <meta property="og:image:height" content="78" />
   <link rel="stylesheet" type="text/css" href="styles/hlm2017.css"/>
