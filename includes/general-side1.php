ac0298cab36357bcccd985e6181d6b3cbe08edb7
Helen Martch <helen@mobilexlabs.com>
2017-08-29 08:28:51 -0500

    fix sears end date, update pdf, add gitlab

--- includes/general-side1.php
+++ includes/general-side1.php
@@ -1,5 +1,6 @@
 <p><img src="/images/facebook-15.png" class="icon" alt=""/>Find me on <a href="http://www.facebook.com/profile.php?id=100000912637008" target="ext">Facebook</a>.</p>
     <p><img src="/images/GitHub-Mark-32px.png" class="icon" alt="" />View my <a href="https://github.com/hmartch" target="ext">GitHub</a> profile.</p>
+    <p><img src="/images/icon-gitlab.png" class="icon" alt="" />View my <a href="https://gitlab.com/hmartch" target="ext">GitLab</a> profile.</p>
     <p><img src="/images/icon-instagram.png" class="icon" alt="" />See my photos on <a href="https://www.instagram.com/helen.martch/" target="ext">Instagram</a>.</p>
 		<p><img src="/images/linkedin-15.png" class="icon" alt="" />Link up on <a href="http://www.linkedin.com/pub/helen-martch/15/885/931" target="ext">LinkedIn</a>.</p>
     <p><img src="/images/icon-stackoverflow.png" class="icon" alt="" />View my <a href="https://stackoverflow.com/users/1267500/redlena" target="ext">Stack Overflow</a> profile.</p>
